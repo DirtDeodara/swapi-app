bd6f62751b39b324064979be778b5fb11005a29c
Dirt Deodara <dirtdeodara@lithia.com>
2021-12-05 18:40:57 -0800

    little bit of css

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,7 +3,7 @@ import "./ListItem.css";
 const ListItem = ({ item }) => {
   return (
     <li className="itemContainer">
-      <p>{item.name}</p>
+      <p className="name">{item.name}</p>
       <p>Gender: {item.gender}</p>
       <p>Hair color: {item.hair_color}</p>
       <p>Height: {item.height}</p>
